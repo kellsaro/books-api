59cb9d8c429e6b79af209c75b23339bfaa2a52c1
kellsaro <kellsaro@gmail.com>
2022-06-08 15:56:12 -0500

    Add comments on top of BooksController's methods

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,18 +3,22 @@ module Api
     class BooksController < ApplicationController
       before_action :set_book, only: %i[show destroy]
 
+      # GET /books
       def index
         @books = Book.all
       end
 
+      # GET /books/:id
       def show; end
       
+      # POST /books
       def create
         @book = Book.new(book_params)
         @book.save!
         render status: :created
       end
 
+      # DELETE /books/:id
       def destroy
         @book.destroy!
         head :no_content
