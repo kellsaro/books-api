8a9bfa700cffb539be0570ccecc743003f0b8481
kellsaro <kellsaro@gmail.com>
2022-06-14 17:22:45 -0500

    Little update on spec example title

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "POST /register" do
-    it 'authenticates the user' do
+    it 'creates the user' do
       post '/api/v1/users/register', params: { user: { username: 'user1', password: 'password_12345' } }
 
       expect(response).to have_http_status(:created)
